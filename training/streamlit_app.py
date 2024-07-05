4ce5f97a14093153e2c9a9bb2a26de1d548f411c
shivankursharma018 <shivankursharma018@gmail.com>
2024-07-05 12:35:02 +0530

    streamlit and pillow libraries updated

--- training/streamlit_app.py
+++ training/streamlit_app.py
@@ -1,26 +1,38 @@
 import streamlit as st
 import tensorflow as tf
-
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
 
-st.setoption('deprecation.showfileUploaderEncoding', False)
-@st.cache(allow_output_mutation=True)
+# Set the page configuration
+st.set_page_config(
+    page_title="Potato Disease Classification",
+    page_icon=":potato:",
+    layout="centered",
+    initial_sidebar_state="expanded",
+)
+
+# Function to load the model
+@st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('D:\_programming\Potato-Disease-Classification-System-using-Convolutional-Neural-Networks-CNN\saved_models\1.keras')
+    model_path = 'D:/_programming/Potato-Disease-Classification-System-using-Convolutional-Neural-Networks-CNN/saved_models/1.keras'
+    model = tf.keras.models.load_model(model_path)
     return model
 
 model = load_model()
+
 st.write("""
          # Potato Disease Classification
          """)
-file = st.file_uploader("Upload a potato leaf image", type=["jpg","png","jpeg"])
+
+# File uploader widget
+file = st.file_uploader("Upload a potato leaf image", type=["jpg", "png", "jpeg"])
+
 def import_and_predict(image_data, model):
-    size = (256,256)
-    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    size = (256, 256)
+    image = ImageOps.fit(image_data, size, Image.Resampling.LANCZOS)
     img = np.asarray(image)
-    img_reshape = img[np.newaxis,...]   #3D -> 4D image
+    img_reshape = img[np.newaxis, ...]  # 3D -> 4D image
     predictions = model.predict(img_reshape)
     class_names = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
     predicted_class = class_names[np.argmax(predictions[0])]
@@ -33,5 +45,5 @@ else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     predicted_class, confidence = import_and_predict(image, model)
-    string = f"predicted label : {predicted_class}\nconfidence : {confidence}%"
+    string = f"Predicted label: {predicted_class}\nConfidence: {confidence}%"
     st.success(string)
